1ed2026e67420fbf743308ec9c0281d3afdf4380
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-05-19 13:16:41 +0200

    update

--- js/app.js
+++ js/app.js
@@ -24,8 +24,8 @@ const trainers = [
         ]
     }
 ];
-
 let game = document.getElementById("trainers");
+let pokemonsBattle = [];
 
 game.innerHTML = `
     ${trainers.map((t, i) => `
@@ -38,35 +38,69 @@ game.innerHTML = `
     `).join('')}
 `;
 
+
+
+
 function selectTrainer(element) {
     let pokemons = trainers[element.getAttribute("data-index")].pokemons;
+    // Savoir si on a cliqué sur un dresseur qu'on avait deja choisi
     // Get a random element from an array
-}
+    // Get pokemon
 
-/*
-for (let i = 0; i < trainers.length; i++) {
-    let trainer = trainers[i];
-    let pokemons = trainer.pokemons;
+    //console.log(pokemons[0]);
 
-    let trainerHtml = document.createElement("div");
-
-    let trainerName = document.createElement("h2");
-    trainerName.textContent = trainer.name;
-
-    trainerHtml.appendChild(trainerName);
-
-    let listPokemons = document.createElement("ul");
-
-    for (let j = 0; j < pokemons.length; j++) {
-        let pokemon = pokemons[j];
-
-        let pokemonLi = document.createElement("li");
-        pokemonLi.textContent = pokemon.name + " hp:" + pokemon.hp;
-
-        listPokemons.appendChild(pokemonLi);
+    if (pokemonsBattle.length < 2) {
+        pokemonsBattle.push(pokemons[0]);
+    } else {
+        battlePok();
     }
 
-    trainerHtml.appendChild(listPokemons);
-    game.appendChild(trainerHtml);
+    console.log(pokemonsBattle);
 }
-*/
+
+let battle = true;
+let attacker = 0;
+let defender = 1;
+
+//function battlePok() {
+    while (battle) {
+        let pokemonAttack = pokemonsBattle[attacker];
+        let pokemonDefend = pokemonsBattle[defender];
+
+        console.log(pokemonAttack);
+
+        /*
+        { name: "Pikachu", type: "electric", hp: 120, attack: 23 }
+        { name: "Dracaufeu", type: "fire", hp: 150, attack: 200 }
+
+
+        Boucle 1:
+            Pika attack 23 Dracaufeu
+            Dracaufeu hp = 150 - 23
+            if (hp dracau <= 0) (--pas fait--)
+                afficher victoire pika
+                battle = false
+
+        Boucle 2:
+            Dracau attack 200 pika
+            Pika hp = 120 - 200
+            if (hp pika <= 0) (--fait--)
+                afficher victoire dracau
+                battle = false
+         */
+
+
+
+
+
+        if (attacker === 0) {
+            attacker = 1;
+            defender = 0;
+        } else {
+            attacker = 0;
+            defender = 1;
+        }
+
+        battle = false;
+    }
+//}
